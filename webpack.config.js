4e00646520e0bf5f17e725add09eb43abe2ec9f0
Rayen Khedirir <Rayen.khediri@esprit.tn>
2026-04-15 19:34:12 +0100

    fixing the clutch

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,59 @@
 const Encore = require('@symfony/webpack-encore');
 
-// Manually configure the runtime environment if not already configured yet by the "encore" command.
-// It's useful when you use tools that rely on webpack.config.js file.
+// Configure runtime environment
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
-    // directory where compiled assets will be stored
+    // Output path
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
     .setPublicPath('/build')
-    
+
     /*
-     * ENTRY CONFIG
+     * ENTRY CONFIG (JS)
      */
     .addEntry('app', './assets/app.js')
     .addEntry('messagerie', './assets/js/messagerie.js')
     .addEntry('sidebar', './assets/js/sidebar.js')
-    
+
     /*
-     * STYLE ENTRIES
+     * STYLE ENTRIES (CSS)
      */
     .addStyleEntry('variables', './assets/css/variables.css')
     .addStyleEntry('admin', './assets/css/admin.css')
     .addStyleEntry('agriculteur', './assets/css/agriculteur.css')
     .addStyleEntry('banque', './assets/css/banque.css')
-    .addStyleEntry('messagerie-css', './assets/css/messagerie.css')
-    .addStyleEntry('sidebar-style', './assets/css/sidebar.css')
-    .addStyleEntry('messagerie-style', './assets/styles/messagerie-style.css')
-    .addStyleEntry('messagerie-style', './assets/css/messagerie.css')
-    .addStyleEntry('sidebar-style', './assets/css/sidebar.css')
+    .addStyleEntry('messagerie_style', './assets/css/messagerie.css')
+    .addStyleEntry('sidebar_style', './assets/css/sidebar.css')
 
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    // Optimization
     .splitEntryChunks()
-
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
 
     /*
-     * FEATURE CONFIG
+     * FEATURES
      */
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
-    // configure Babel
+    // Babel config
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-proposal-class-properties');
     })
 
-    // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
 
-    // enables Sass/SCSS support
+    // Loaders
     .enableSassLoader()
-
-    // enables PostCSS support
     .enablePostCssLoader()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
-
-    // uncomment if you use React
-    //.enableReactPreset()
-
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
-
-    // uncomment if you're having problems with a jQuery plugin
+    // jQuery support
     .autoProvidejQuery()
 ;
 
